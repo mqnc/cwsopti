6c7edcce36d9409117bfbea8cacd18c0ce66b758
Mirko <mirko.kunze@web.de>
2018-09-20 00:02:37 +0200

    reading and writing ply

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,25 @@ using namespace std;
 using namespace Eigen;
 using namespace tinyply;
 
+inline double h_(Vec n){
+	return n.x() * n.y() * n.z();
+}
+
+inline double h(Vec n, double r){
+	return (h_(n) -h_(-n))/2.0 + r;
+}
+
 int main()
 {
-	read_ply_file("ico6.ply");
+	auto mesh = read_ply_file("ico8.ply");
 
+
+	for(auto& v:mesh.v){
+		v = v*h(v, 0.1);	
+	}
+
+
+	write_ply_file("output.ply", mesh);
 #ifdef _DEBUG
 	system("PAUSE");
 #endif
