94b01477fdcef76f2f47143b6a03d13ffe57da6c
mqnc <mirko.kunze@web.de>
2018-09-19 15:48:45 +0200

    parsing ply and converting to mesh struct

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,17 @@ using namespace tinyply;
 
 using Vec = Vector3d;
 using Mat = Matrix3d;
+using Tri = array<uint32_t, 3>;
 
-void read_ply_file(const std::string & filepath)
+struct Mesh{
+	vector<Vec> v;
+	vector<Tri> f;
+};
+
+Mesh read_ply_file(const std::string & filepath)
 {
+	Mesh result;
+
 	try
 	{
 		std::ifstream ss(filepath, std::ios::binary);
@@ -47,26 +55,48 @@ void read_ply_file(const std::string & filepath)
 			struct float3 { float x, y, z; };
 			std::vector<float3> vbuffer(vertices->count);
 			std::memcpy(vbuffer.data(), vertices->buffer.get(), vertices->buffer.size_bytes());
+
+			result.v.resize(vertices->count);
+			for (uint32_t i=0; i<vertices->count; i++){
+				result.v[i] << vbuffer[i].x, vbuffer[i].y, vbuffer[i].z;
+				result.v[i].normalize();
+			}
 		}
 		if (vertices->t == Type::FLOAT64) {
 			struct double3 { double x, y, z; };
 			std::vector<double3> vbuffer(vertices->count);
 			std::memcpy(vbuffer.data(), vertices->buffer.get(), vertices->buffer.size_bytes());
+
+			result.v.resize(vertices->count);
+			for (uint32_t i=0; i<vertices->count; i++){
+				result.v[i] << vbuffer[i].x, vbuffer[i].y, vbuffer[i].z;
+				result.v[i].normalize();
+			}
 		}
 
-		struct uint3 { uint32_t x, y, z; };
-		std::vector<uint3> fbuffer(faces->count);
+		struct triangle { uint32_t v1, v2, v3; };
+		std::vector<triangle> fbuffer(faces->count);
 		std::memcpy(fbuffer.data(), faces->buffer.get(), faces->buffer.size_bytes());
 
+		result.f.resize(faces->count);
+		for (uint32_t i=0; i<faces->count; i++){
+			result.f[i] = {fbuffer[i].v1, fbuffer[i].v2, fbuffer[i].v3};
+		}
 	}
 	catch (const std::exception & e)
 	{
 		std::cerr << "Caught tinyply exception: " << e.what() << std::endl;
 	}
+
+	return result;
 }
 
 int main()
 {
 	read_ply_file("ico6.ply");
+
+#ifdef _DEBUG
+	system("PAUSE");
+#endif
 }
 
